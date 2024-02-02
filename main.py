40be26741bbb1ce0100726234bb09fcb03c8867d
AnnoyingShpee <74668207+AnnoyingShpee@users.noreply.github.com>
2024-02-02 16:35:05 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import random as r
+
+r.seed(935)
+template_tiles = [[8, 7, 6],
+                  [5, 4, 3],
+                  [2, 1, 0]]
+goal_state = [1, 1,
+              [[1, 2, 3],
+              [8, 0, 4],
+              [7, 6, 5]]
+              ]
+
+max_runs = 10
+completed_runs = 0
+
+def shuffle_tiles():
+    random.shuffle(template_tiles)
+    return
+
+def main():
+    return
+
+def check_state():
+    return
+
+if __name__ == '__main__':
+    main()
+
